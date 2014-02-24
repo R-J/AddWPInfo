bf56b026875b0493a09609725e874f49ef196faa
R-J <robin.jurinka@gmx.net>
2014-02-24 19:47:41 +0100

    Create settings.php

---
+++ views/settings.php
@@ -0,0 +1,18 @@
+<?php defined('APPLICATION') or exit();?>
+
+<h1><?php echo $this->Data('Title');?></h1>
+<div class="Info"><?php echo T('WordPress Info Settings', 'WordPress Info shows additional input fields in new discussions.');?></div>
+<?php
+echo $this->Form->Open();
+echo $this->Form->Errors();
+
+echo '<hr />';
+echo $this->Form->Label('Please mark the categories to <strong>exclude</strong> from showing those fields.', 'Plugins.WPInfo.CategoryIDs');
+echo $this->Form->CheckBoxList('Plugins.WPInfo.CategoryIDs', $this->CategoryData, $this->ExcludeCategory, array('ValueField' => 'CategoryID', 'TextField' => 'Name'));
+
+echo '<hr />';
+echo $this->Form->CheckBox('Plugins.WPInfo.KeepTags', T('Allow additional tags'));
+
+echo '<hr />';
+echo $this->Form->Button('Save');
+echo $this->Form->Close();
