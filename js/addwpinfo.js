b0722477446d6b1243b90e6fb1f915b1d2927a0f
R-J <robin.jurinka@gmx.net>
2014-02-26 22:20:08 +0100

    Update addwpinfo.js
    Changed code for better compatibility to jquery plugin Select2

--- js/addwpinfo.js
+++ js/addwpinfo.js
@@ -1,8 +1,8 @@
 // toggle visibility of #AddWPInfos depending on #Form_CategoryID
 $(document).ready(function() {
    var addWPInfoCategoryIDs = jQuery.parseJSON(gdn.definition('AddWPInfoCategoryIDs'));
-   $('#Form_CategoryID').change(function() {
-      if (addWPInfoCategoryIDs.indexOf($('#Form_CategoryID').val()) > -1) {
+   $('#Form_CategoryID').on('change', function(e) {
+      if (addWPInfoCategoryIDs.indexOf(e.target.value) > -1) {
          $('#AddWPInfo').addClass('Hidden');
       } else {
          $('#AddWPInfo').removeClass('Hidden');
